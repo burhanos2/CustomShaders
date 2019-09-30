1f7ef642e0d8adad8cc67b35f0ea15a9a003676c
burhanos2 <24742@ma-web.nl>
2019-09-30 13:49:04 +0200

    created base, updated readme
    it has been a very busy week for me in my personal life, so I forgot to commit a lot

---
+++ Assets/Scripts/Presso.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presso : MonoBehaviour
+{
+    public Renderer rend;
+    private Material mat;
+
+    private void Awake()
+    {
+        mat = rend.material;    
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            mat.SetVector("_Color", new Vector4(0f,0f,0f,0f));
+        }
+    }
+}
